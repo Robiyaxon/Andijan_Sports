cbffba3a0bc8d4f07f1feaabcd991d3e87aabc5f
Robiyaxon <tuhtaevarobiahon@gmail.com>
2022-11-12 19:17:39 +0500

    pull

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -8,7 +8,7 @@ import {
   UserOutlined,
 } from '@ant-design/icons'
 import { Layout, Menu } from 'antd'
-import { NavLink, Route, Routes, useParams } from 'react-router-dom'
+import { NavLink, Route, Routes } from 'react-router-dom'
 
 import img from '../../assets/images/sidebar_bg.png'
 import logo from '../../assets/images/logo.png'
@@ -21,7 +21,19 @@ import Akademiya from './../akademiya/Akademiya'
 import styles from './Sidebar.module.css'
 import Homiy from '../invester/Homiy'
 import Footer from '../footer/Footer'
-
+import dokon1 from "../../assets/dokon1.png"
+import dokon2 from "../../assets/dokon4.png"
+import dokon3 from "../../assets/img1dokon.png"
+import dokon4 from "../../assets/product1.png"
+import dokon5 from "../../assets/product2.png"
+import dokon6 from "../../assets/product3.png"
+import dokon7 from "../../assets/product5.png"
+import dokon8 from "../../assets/dokon1.png"
+import dokon9 from "../../assets/dokon4.png"
+import dokon10 from "../../assets/img1dokon.png"
+import dokon11 from "../../assets/product6.png"
+import dokon12 from "../../assets/product8.png"
+import { DokonSingle } from './DokonSingle'
 export const Sidebar2 = () => {
   const [navLinksClass, setNavLinksClass] = useState('')
   const [linksClass, setLinksClass] = useState('')
@@ -96,18 +108,18 @@ const items = [
   getItem('Akademiya', '7', <FileOutlined />, '/academy'),
 ]
 const map = [
-  { id: 1, title: "Lorem Ipsum is simply dummy text of the printing and typesetting industry.", name: "FC ANDIJON TRIKOT HOME 22/23" },
-  { id: 2, title: "Lorem Ipsum is simply dummy text of the printing and typesetting industry." },
-  { id: 3, title: "Lorem Ipsum is simply dummy text of the printing and typesetting industry." },
-  { id: 4, title: "Lorem Ipsum is simply dummy text of the printing and typesetting industry." },
-  { id: 5, title: "Lorem Ipsum is simply dummy text of the printing and typesetting industry." },
-  { id: 6, title: "Lorem Ipsum is simply dummy text of the printing and typesetting industry." },
-  { id: 8, title: "Lorem Ipsum is simply dummy text of the printing and typesetting industry." },
-  { id: 9, title: "Lorem Ipsum is simply dummy text of the printing and typesetting industry." },
-  { id: 10, title: "Lorem Ipsum is simply dummy text of the printing and typesetting industry." },
-  { id: 11, title: "Lorem Ipsum is simply dummy text of the printing and typesetting industry." },
-  { id: 12, title: "Lorem Ipsum is simply dummy text of the printing and typesetting industry." },
-  { id: 13, title: "Lorem Ipsum is simply dummy text of the printing and typesetting industry." },
+  { id: 1, title: "Lorem Ipsum is simply dummy text of the printing and typesetting industry.", name: "FC ANDIJON TRIKOT HOME 22/23", img: {dokon1} },
+  { id: 3, title: "Lorem Ipsum is simply dummy text of the printing and typesetting industry.", img:dokon2 },
+  { id: 4, title: "Lorem Ipsum is simply dummy text of the printing and typesetting industry.", img:dokon3 },
+  { id: 5, title: "Lorem Ipsum is simply dummy text of the printing and typesetting industry.", img:dokon4 },
+  { id: 6, title: "Lorem Ipsum is simply dummy text of the printing and typesetting industry.", img:dokon5},
+  { id: 8, title: "Lorem Ipsum is simply dummy text of the printing and typesetting industry.", img:dokon6 },
+  { id: 9, title: "Lorem Ipsum is simply dummy text of the printing and typesetting industry.", img:dokon7 },
+  { id: 10, title: "Lorem Ipsum is simply dummy text of the printing and typesetting industry.", img:dokon8 },
+  { id: 11, title: "Lorem Ipsum is simply dummy text of the printing and typesetting industry.", img:dokon9 },
+  { id: 12, title: "Lorem Ipsum is simply dummy text of the printing and typesetting industry.", img:dokon10 },
+  { id: 13, title: "Lorem Ipsum is simply dummy text of the printing and typesetting industry.", img:dokon11 },
+  { id: 2, title: "Lorem Ipsum is simply dummy text of the printing and typesetting industry.", img:dokon12 },
 ]
 const dataMap = items.map((d) => (
   <Menu.Item key={d.key}>
@@ -167,12 +179,6 @@ const Sidebar = () => {
         </Content>
       </Layout>
     </Layout>
-  )
-}
-const DokonSingle = (props) => {
-  let { id } = useParams()
-  return (
-    <div>img</div>
   )
 }
 export default Sidebar
